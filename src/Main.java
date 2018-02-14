4416b2b5c8844d48a31fb8d5440fa03568915822
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-14 11:08:27 +0100

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,8 +13,24 @@ public class Main {
         map.put("17", "Ed");
         map.put("29", "Ed");
         map.put("56", "Ed");
+
+        System.out.println(reverse(map));
     }
-    public static void reverse(Map<String,String> map){
+    public static Map<String, Set<String>> reverse(Map<String,String> map){
         Map<String,Set<String>> value = new HashMap<>();
+
+        for (Map.Entry<String,String> m: map.entrySet()){
+            if (value.containsKey(m.getValue())){
+                Set<String> set = value.get(m.getValue());
+                set.add(m.getKey());
+                value.put(m.getValue(), set);
+            } else {
+                Set<String> set = new HashSet<>();
+                set.add(m.getKey());
+                value.put(m.getValue(), set);
+            }
+        }
+        return value;
     }
+
 }
